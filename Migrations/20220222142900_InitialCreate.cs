e3cb28a19a1339348da9c0904283f5e2677a4100
Rasmus Fogelberg <fogelbergrasmus@gmail.com>
2022-02-22 15:31:58 +0100

    Update migrations

--- Migrations/20220222142900_InitialCreate.cs
+++ Migrations/20220222142900_InitialCreate.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -40,7 +41,8 @@ namespace DiscoSaurus.Migrations
                 {
                     UserId = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -56,7 +58,9 @@ namespace DiscoSaurus.Migrations
                     GenreId = table.Column<int>(type: "int", nullable: false),
                     ArtistId = table.Column<int>(type: "int", nullable: false),
                     Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "bit", nullable: false),
+                    LentToUser = table.Column<int>(type: "int", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -73,6 +77,50 @@ namespace DiscoSaurus.Migrations
                         principalTable: "Genres",
                         principalColumn: "GenreId",
                         onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BorrowedItem",
+                columns: table => new
+                {
+                    BorrowedItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: true),
+                    AlbumId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BorrowedItem", x => x.BorrowedItemId);
+                    table.ForeignKey(
+                        name: "FK_BorrowedItem_Albums_AlbumId",
+                        column: x => x.AlbumId,
+                        principalTable: "Albums",
+                        principalColumn: "AlbumId");
+                    table.ForeignKey(
+                        name: "FK_BorrowedItem_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Borrowed",
+                columns: table => new
+                {
+                    BorrowedId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BorrowedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    BorrowedItemId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Borrowed", x => x.BorrowedId);
+                    table.ForeignKey(
+                        name: "FK_Borrowed_BorrowedItem_BorrowedItemId",
+                        column: x => x.BorrowedItemId,
+                        principalTable: "BorrowedItem",
+                        principalColumn: "BorrowedItemId");
                 });
 
             migrationBuilder.InsertData(
@@ -99,24 +147,24 @@ namespace DiscoSaurus.Migrations
 
             migrationBuilder.InsertData(
                 table: "Users",
-                columns: new[] { "UserId", "Name" },
+                columns: new[] { "UserId", "Password", "Username" },
                 values: new object[,]
                 {
-                    { 1, "John Doe" },
-                    { 2, "Jane Smith" },
-                    { 3, "Moby Dick" }
+                    { 1, "password", "Mattias" },
+                    { 2, "password123", "Malin" },
+                    { 3, "123password", "Lars" }
                 });
 
             migrationBuilder.InsertData(
                 table: "Albums",
-                columns: new[] { "AlbumId", "ArtistId", "GenreId", "Price", "Title" },
+                columns: new[] { "AlbumId", "ArtistId", "GenreId", "IsAvailable", "LentToUser", "Price", "Title" },
                 values: new object[,]
                 {
-                    { 1, 1, 1, 15.99m, "Thunderstruck" },
-                    { 2, 2, 2, 25.50m, "Balls to the Wall" },
-                    { 3, 3, 3, 13.60m, "The Number of the Beast" },
-                    { 4, 4, 1, 16.15m, "We Like It Here" },
-                    { 5, 5, 3, 39.99m, "Volume Rock" }
+                    { 1, 1, 1, true, null, 15.99m, "Thunderstruck" },
+                    { 2, 2, 2, true, null, 25.50m, "Balls to the Wall" },
+                    { 3, 3, 3, true, null, 13.60m, "The Number of the Beast" },
+                    { 4, 4, 1, true, null, 16.15m, "We Like It Here" },
+                    { 5, 5, 3, true, null, 39.99m, "Volume Rock" }
                 });
 
             migrationBuilder.CreateIndex(
@@ -128,10 +176,31 @@ namespace DiscoSaurus.Migrations
                 name: "IX_Albums_GenreId",
                 table: "Albums",
                 column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Borrowed_BorrowedItemId",
+                table: "Borrowed",
+                column: "BorrowedItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowedItem_AlbumId",
+                table: "BorrowedItem",
+                column: "AlbumId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BorrowedItem_UserId",
+                table: "BorrowedItem",
+                column: "UserId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "Borrowed");
+
+            migrationBuilder.DropTable(
+                name: "BorrowedItem");
+
             migrationBuilder.DropTable(
                 name: "Albums");
 
